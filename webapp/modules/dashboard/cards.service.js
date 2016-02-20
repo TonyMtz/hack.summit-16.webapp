61d1dd6e7f098f79aafa8bc995f7c1c580d8e6d5
tonymtz <tmartinez@switchfly.com>
2016-02-20 13:44:45 -0700

    url in a config as ng-constant

--- webapp/modules/dashboard/cards.service.js
+++ webapp/modules/dashboard/cards.service.js
@@ -1,19 +1,21 @@
 let moduleName = 'dashboard.services';
 
 const HTTP = new WeakMap();
+const CONFIG = new WeakMap();
 
 export default class CardService {
-  constructor($http) {
+  constructor($http, AppConfig) {
     HTTP.set(this, $http);
+    CONFIG.set(this, AppConfig);
   }
 
   getCards() {
-    return HTTP.get(this).get('http://169.55.85.55:9000/cards').then(result => result.data);
+    return HTTP.get(this).get(CONFIG.get(this).host + '/cards').then(result => result.data);
   }
 
-  static cardServiceFactory ($http) {
-    return new CardService($http);
+  static cardServiceFactory($http, AppConfig) {
+    return new CardService($http, AppConfig);
   }
 }
 
-CardService.cardServiceFactory.$inject = ['$http'];
+CardService.cardServiceFactory.$inject = ['$http', 'AppConfig'];
--- webapp/modules/dashboard/dashboard.module.js
+++ webapp/modules/dashboard/dashboard.module.js
@@ -2,12 +2,15 @@ import routes from './dashboard.routes.js';
 import DashboardController from './dashboard.controller.js';
 import CardService from './cards.service.js';
 
+import config from './config.json';
+
 export default function dashboard(angular, ...args) {
 
   return angular.module('app.dashboard', args)
     .config(routes)
     .controller('DashboardController', DashboardController)
     .factory('CardService', CardService.cardServiceFactory)
+    .constant('AppConfig', config)
     .name;
 
 };
