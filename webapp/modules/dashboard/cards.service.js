39d24786107c19e427a5b9a261dedbebf7cb02b1
tonymtz <tmartinez@switchfly.com>
2016-02-21 19:01:09 -0700

    Displaying cards

--- webapp/modules/dashboard/cards.service.js
+++ webapp/modules/dashboard/cards.service.js
@@ -1,19 +1,22 @@
 const HTTP = new WeakMap();
 const CONFIG = new WeakMap();
+const COOKIES = new WeakMap();
 
 export default class CardService {
-  constructor($http, AppConfig) {
+  constructor($http, AppConfig, $cookies) {
     HTTP.set(this, $http);
     CONFIG.set(this, AppConfig);
+    COOKIES.set(this, $cookies);
   }
 
   getCards() {
-    return HTTP.get(this).get(CONFIG.get(this).host + '/cards').then(result => result.data);
+    let xToken = COOKIES.get(this).get('x-token');
+    return HTTP.get(this).get(CONFIG.get(this).host + '/cards/' + xToken).then(result => result.data);
   }
 
-  static cardServiceFactory($http, AppConfig) {
-    return new CardService($http, AppConfig);
+  static cardServiceFactory($http, AppConfig, $cookies) {
+    return new CardService($http, AppConfig, $cookies);
   }
 }
 
-CardService.cardServiceFactory.$inject = ['$http', 'AppConfig'];
+CardService.cardServiceFactory.$inject = ['$http', 'AppConfig', '$cookies'];
